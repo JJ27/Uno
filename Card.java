680d66a6d01a42dfc7e0d4a005decb6812a49a7d
JoshJ <hellojosh04@gmail.com>
2020-09-05 17:38:42 -0400

    Setup Card File

--- Card.java
+++ Card.java
@@ -1,5 +1,26 @@
 public class Card{
-  public Card{
-    //constructor stuff
+  private String color;
+  private int n;
+  private String action;
+  public Card(String color, int n, String action){
+    this.color = color;
+    this.n = n;
+    this.action = action;
+  }
+  public String getColor(){
+    return color;
+  }
+  public int getNum(){
+    return n;
+  }
+  public String getAction(){
+    return action;
+  }
+  public static boolean check(Card other){
+    if(color.equals(other.getColor()))
+      return true;
+  }
+  public String toString(){
+    return action + " " + color + " " + n;
   }
 }
